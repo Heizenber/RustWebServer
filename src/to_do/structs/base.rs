891ac25f7ede55bbb10b2fb8fbc1cd00c107fbe3
Heizenberg <popov.roman91@gmail.com>
2023-09-08 22:37:30 +0300

    added serialization to Base struct and to_do_items

--- src/to_do/structs/base.rs
+++ src/to_do/structs/base.rs
@@ -1,3 +1,7 @@
+use serde::Serialize;
+
+
+#[derive(Serialize)]
 pub struct Base {
     pub title: String,
     pub status: String,
--- src/views/to_do/get.rs
+++ src/views/to_do/get.rs
@@ -1,8 +1,17 @@
 use actix_web::{web, Responder};
-use serde_json::{Map, value::Value};
 use crate::state::read_file;
+use crate::to_do::{ItemTypes, to_do_factory};
+use crate::json_serialization::to_do_items::ToDoItems;
 
 pub async fn get() -> impl Responder {
     let state = read_file("./state.json");
-    web::Json(state)
+    let mut array_buffer = vec![];
+
+    for (key, value) in state {
+        let item_type = value.to_string();
+        let item = to_do_factory(&item_type, &key).unwrap();
+        array_buffer.push(item);
+    }
+    let return_package: ToDoItems = ToDoItems::new(array_buffer);
+    web::Json(return_package)
 }
