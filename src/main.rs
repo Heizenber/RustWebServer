be09cfc932805d817f0a109a9c96e21d6e2dc629
Heizenberg <popov.roman91@gmail.com>
2023-09-08 19:07:12 +0300

    added actix-web and views to the project

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,17 @@
-mod processes;
+use actix_web::{App, HttpServer};
 mod state;
 mod to_do;
+mod views;
+mod processes;
 
-use processes::process_input;
-use serde_json::value::Value;
-use serde_json::Map;
-use state::read_file;
-use std::env;
-use to_do::to_do_factory;
-
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let command: &String = &args[1];
-    let title: &String = &args[2];
-
-    let state: Map<String, Value> = read_file("./state.json");
-
-    let status: String;
-
-    match &state.get(*&title) {
-        Some(result) => {
-            status = result.to_string().replace('\"', "");
-        }
-        None => {
-            status = "pending".to_string();
-        }
-    }
-
-    let item = to_do_factory(&status, title).expect(&status);
-    process_input(item, command.to_string(), &state);
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let app = App::new().configure(
+            views::views_factory
+        );
+        app
+    }).bind("127.0.0.1:8000")?
+      .run()
+      .await
 }
--- src/state.rs
+++ src/state.rs
@@ -1,6 +1,5 @@
 use std::fs;
-use std::fs::File;
-use std::io::Read;
+
 
 use serde_json::json;
 use serde_json::value::Value;
@@ -18,7 +17,6 @@ pub fn write_to_file(file_name: &str, state: &mut Map<String, Value>) {
     let new_data = json!(state);
     fs::write(file_name.to_string(), new_data.to_string()).expect("Unable to write file");
 }
-
 
 #[cfg(test)]
 mod tests {
