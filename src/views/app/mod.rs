ea636cb2cb4a2c40763fc433480a3815c4e2dd7b
Heizenberg <popov.roman91@gmail.com>
2023-09-21 19:53:10 +0300

    added several html views

---
+++ src/views/app/mod.rs
@@ -0,0 +1,10 @@
+
+use actix_web::web;
+mod items;
+use super::path::Path;
+
+pub fn app_factory(app: &mut web::ServiceConfig) {
+    let base_path: Path = Path {prefix: "/".to_string()};
+    app.route(&base_path.define("".to_string()),
+            web::get().to(items::items));
+}
